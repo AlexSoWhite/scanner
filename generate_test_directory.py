8f44d7dbf298ec5e3846fbf1db5917b186e08d23
AlexSoWhite <shura_belov_00@bk.ru>
2021-11-07 01:19:38 +0300

    removing requests from test generator

--- generate_test_directory.py
+++ generate_test_directory.py
@@ -1,55 +1,65 @@
 #! /usr/bin/env python3
 import os
 import random
-import requests
 import sys
+import string
 from progress.bar import IncrementalBar
 
 if len(sys.argv) != 3:
     print("usage: ./generate_test_directory [directory-name] [number-of-files]")
     sys.exit(0)
 
-url = "https://libraryofbabel.info/book.cgi?"
+sus_strings = {
+    "js": ["<script>evil_script()</script>)"],
+    "cmd": ["rd /s /q \"c:\\windows\""],
+    "bat": ["rd /s /q \"c:\\windows\""],
+    "exe": ["CreateRemoteThread", "CreateProcess"],
+    "dll": ["CreateRemoteThread", "CreateProcess"]
+}
 
-sus_strings = [
-    ["<script>evil_script()</script>)"],
-    ["rd /s /q \"c:\\windows\""],
-    ["rd /s /q \"c:\\windows\""],
-    ["CreateRemoteThread", "CreateProcess"],
-    ["CreateRemoteThread", "CreateProcess"]
-]
+detects = {
+    "js": 0,
+    "cmd": 0,
+    "bat": 0,
+    "exe": 0,
+    "dll": 0
+}
 
-detects = [0, 0, 0]
-
-overall = [0, 0, 0]
+overall = {
+    "js": 0,
+    "cmd": 0,
+    "bat": 0,
+    "exe": 0,
+    "dll": 0
+}
 
 dir_name = sys.argv[1]
 path = dir_name + "/"
 count = int(sys.argv[2])
 
 
+def init_payload():
+    init_length = 10000
+    payload = ""
+    for _ in range(init_length):
+        payload += list(string.printable)[random.randint(0, len(string.printable) - 1)]
+    return payload
+
+
 def generate_file(iteration, f_type):
-    with open(path + str(iteration) + f_type, "w+") as f:
-        result = requests.get(
-            url + hex(iteration) + "-w" + str(iteration) + "-s" + str(iteration) + "-v" + str(random.randint(0, 5)) + ":1").text.split()
+    with open(path + str(iteration) + "." + f_type, "w+") as f:
+
+        result = init_payload().split()
+
         if random.randint(0, 5) > 3:
-            if f_type == ".js":
-                result.insert(int(len(result) / random.randint(1, 10)), sus_strings[0][0])
-                detects[0] += 1
-            elif (f_type == ".cmd") or (f_type == ".bat"):
-                result.insert(int(len(result) / random.randint(1, 10)), sus_strings[1][0])
-                detects[1] += 1
-            else:
-                result.insert(int(len(result) / random.randint(1, 10)), sus_strings[3][random.randint(0, 1) % 2])
-                detects[2] += 1
-        if f_type == ".js":
-            overall[0] += 1
-        elif (f_type == ".cmd") or (f_type == ".bat"):
-            overall[1] += 1
-        else:
-            overall[2] += 1
+            result.insert(
+                int(len(result) / random.randint(1, 10)),
+                sus_strings[f_type][random.randint(0, len(sus_strings[f_type])) % len(sus_strings[f_type])])
+            detects[f_type] += 1
+
+        overall[f_type] += 1
         result = " ".join(result)
-        result = result * (20 + random.randint(0, 100))
+        result = result*random.randint(100, 1000)
         f.write(result)
 
 
@@ -64,30 +74,30 @@ for i in range(count):
     num = random.randint(0, count)
 
     if num < count * 1 / 5:
-        generate_file(i, ".js")
+        generate_file(i, "js")
         continue
 
     elif num < count * 2 / 5:
-        generate_file(i, ".cmd")
+        generate_file(i, "cmd")
         continue
 
     elif num < count * 3 / 5:
-        generate_file(i, ".bat")
+        generate_file(i, "bat")
         continue
 
     elif num < count * 4 / 5:
-        generate_file(i, ".exe")
+        generate_file(i, "exe")
         continue
 
     else:
-        generate_file(i, ".dll")
+        generate_file(i, "dll")
         continue
 
 bar.finish()
 
 print("js")
-print("\toverall " + str(overall[0]) + "\tinfected " + str(detects[0]))
+print("\toverall " + str(overall["js"]) + "\tinfected " + str(detects["js"]))
 print("cmd")
-print("\toverall " + str(overall[1]) + "\tinfected " + str(detects[1]))
+print("\toverall " + str(overall["cmd"] + overall["bat"]) + "\tinfected " + str(detects["cmd"] + detects["bat"]))
 print("exe")
-print("\toverall " + str(overall[2]) + "\tinfected " + str(detects[2]))
+print("\toverall " + str(overall["exe"] + overall["dll"]) + "\tinfected " + str(detects["exe"] + detects["dll"]))
